0707e066e78b0edb0ab8ec78baa95f97fc1949bc
fdoargolo <felipeargolo75@gmail.com>
2024-05-02 00:02:28 -0300

    Pequena prática

--- udemyExercises/src/vectorExercises/ExerciseEven.java
+++ udemyExercises/src/vectorExercises/ExerciseEven.java
@@ -11,12 +11,13 @@ public class ExerciseEven {
 		int n = scanner.nextInt();
 		
 		int[] numeros = new int[n];
-		int pares = 0;
-		int count = 0;
+		int pares = 0, count = 0;
 		
 		for(int i = 0; i<numeros.length; i++) {
+			
 			System.out.print("Digite um numero:");
 			numeros[i] = scanner.nextInt();
+			
 			if(numeros[i]%2 == 0) {
 				pares += numeros[i];
 				count++;
